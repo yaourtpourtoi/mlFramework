871b27e458b9b8734eb496c0f57f5e154e404c00
Markus Spanring <markusspanring@gmail.com>
2018-05-23 12:43:48 +0200

    -n Adding new model using tanh and less aggressive dropout; Increased early stopping

--- conf/keras_models.py
+++ conf/keras_models.py
@@ -140,4 +140,21 @@ def smhtt_dropout(num_inputs, num_outputs):
 
     model.compile(loss="mean_squared_error", optimizer=Nadam(), metrics=['categorical_accuracy'])
     return model
+    
+def smhtt_dropout_tanh(num_inputs, num_outputs):
+    model = Sequential()
+
+    for i, nodes in enumerate([200] * 2):
+        if i == 0:
+            model.add(Dense(nodes, kernel_regularizer=l2(1e-5), input_dim=num_inputs))
+        else:
+            model.add(Dense(nodes))
+        model.add(Activation("tanh"))
+        model.add(Dropout(0.3))
+
+    model.add(Dense(num_outputs, kernel_regularizer=l2(1e-5)))
+    model.add(Activation("softmax"))
+
+    model.compile(loss="kullback_leibler_divergence", optimizer=Adam(lr=1e-4), metrics=['categorical_accuracy'])
+    return model
 
