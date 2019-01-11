e0d4fa35a11f9fe279a6f4f68e4ac08e5fa4d3cb
Markus Spanring <markusspanring@gmail.com>
2019-01-11 10:52:15 +0100

    Modifications to run on KIT training

--- run_model.py
+++ run_model.py
@@ -4,6 +4,7 @@ import pandas
 import json
 import sys
 import os
+from glob import glob
 import argparse
 import cPickle
 import subprocess as sp
@@ -18,11 +19,12 @@ def main():
     parser.add_argument('-t', dest='train',   help='Train new model' , action='store_true')
     parser.add_argument('-s', dest='short',   help='Do !!NOT!! predict shapes' , action='store_true')
     parser.add_argument('-d', dest='datacard',  help='Only produce Datacard' , action='store_true')
-    parser.add_argument('-e', dest='era',  help='Era' , default = "2016")
+    parser.add_argument('-e', dest='era',  help='Era' , choices=["2016","2017"], required = True)
     parser.add_argument('--add_nominal', dest='add_nom',  help='Add nominal samples to prediction', action='store_true' )    
     args = parser.parse_args()
 
     print "---------------------------"
+    print "Era: ", args.era
     print "Running over {0} samples".format(args.channel)
     print "Using {0}".format(args.model), keras.__version__
     if args.train:
@@ -60,10 +62,12 @@ def run(samples,channel, era, use, train,short, datacard = False, add_nominal=Fa
 
     target_names = read.config["target_names"]
     variables = read.config["variables"]
-    if not os.path.exists("models"):
-        os.mkdir("models")
 
-    modelname = "models/{0}.{1}".format(channel,use)
+    models_folder = era + "/models"
+    if not os.path.exists(models_folder):
+        os.makedirs(models_folder)
+
+    modelname = "{0}/{1}.{2}".format(models_folder,channel,use)
     scaler = None
 
     if train:
@@ -75,8 +79,10 @@ def run(samples,channel, era, use, train,short, datacard = False, add_nominal=Fa
         scaler = trainScaler(trainSet, variables )
 
         print " done. Dumping for later."
-        with open("models/StandardScaler.{0}.pkl".format(channel), 'wb') as FSO:
+        with open("{0}/StandardScaler.{1}.pkl".format(models_folder,channel), 'wb') as FSO:
             cPickle.dump(scaler, FSO , 2)
+        scaler = [scaler, scaler] # Hotfix since KIT uses 2 scalers
+
         trainSet = applyScaler(scaler, trainSet, variables)
 
         model = modelObject( parameter_file = parameters,
@@ -86,50 +92,75 @@ def run(samples,channel, era, use, train,short, datacard = False, add_nominal=Fa
         model.save(modelname)
 
     elif not datacard:
-        if os.path.exists("models/StandardScaler.{0}.pkl".format(channel) ):
+        # TODO: Maybe not needed to check. Just load what is there
+        if os.path.exists("{0}/StandardScaler.{1}.pkl".format(models_folder,channel) ):
             print "Loading Scaler"
-            with open( "models/StandardScaler.{0}.pkl".format(channel), "rb" ) as FSO:
-                scaler = cPickle.load( FSO )
+            scaler = []
+            if glob("{0}/{1}_*_keras_preprocessing.pickle".format(models_folder,channel)) :
+                with open( "{0}/{1}_fold0_keras_preprocessing.pickle".format(models_folder,channel), "rb" ) as FSO:
+                    scaler.append( cPickle.load( FSO ) )
+
+                with open( "{0}/{1}_fold1_keras_preprocessing.pickle".format(models_folder,channel), "rb" ) as FSO:
+                    scaler.append( cPickle.load( FSO ) )
+            else:
+                with open( "{0}/StandardScaler.{1}.pkl".format(models_folder,channel), "rb" ) as FSO:
+                    tmp = cPickle.load( FSO )
+                    scaler = [tmp,tmp]
+
 
         print "Loading model and predicting."
         model = modelObject( filename = modelname )
+        read.variables = model.variables
+        variables = model.variables
 
     if not datacard:
 
+        # Change here path to where you want to write your predictions
+        outpath = "/afs/hephy.at/data/mspanring01/predictions_" + era
         predictions = {}
         print "Predicting samples"
         if add_nominal:
             print "Predicting Nominal"
             for sample, sampleConfig in read.get(what = "nominal", for_prediction = True):
-                sandbox(channel, model, scaler, sample, variables, "nom_" + sampleConfig["histname"] ,sampleConfig, read.modifyDF )   
+                sandbox(channel, model, scaler, sample, variables, "nom_" + sampleConfig["histname"] ,sampleConfig, read.modifyDF, outpath )   
 
 
         for sample, sampleConfig in read.get(what = "full", add_jec = not short, for_prediction = True):
             if "data" in sampleConfig["histname"]:
-                sandbox(channel, model, scaler, sample, variables, "NOMINAL_ntuple_Data",sampleConfig, read.modifyDF)
+                sandbox(channel, model, scaler, sample, variables, "NOMINAL_ntuple_Data",sampleConfig, read.modifyDF, outpath)
             elif "full" in sampleConfig["histname"]:
-                sandbox(channel, model, scaler, sample, variables,  "NOMINAL_ntuple_" + sampleConfig["histname"].split("_")[0], sampleConfig, read.modifyDF )
+                sandbox(channel, model, scaler, sample, variables,  "NOMINAL_ntuple_" + sampleConfig["histname"].split("_")[0], sampleConfig, read.modifyDF, outpath )
             else:
                 splName = sampleConfig["histname"].split("_")
-                sandbox(channel, model, scaler, sample, variables,  "_".join(splName[1:])+"_ntuple_" + sampleConfig["histname"].split("_")[0], sampleConfig, read.modifyDF )
+                sandbox(channel, model, scaler, sample, variables,  "_".join(splName[1:])+"_ntuple_" + sampleConfig["histname"].split("_")[0], sampleConfig, read.modifyDF, outpath )
 
         if not short:
-            print "Predicting TES shapes"
+            print "Predicting shapes"
             for sample, sampleConfig in read.get(what = "tes", for_prediction = True):
-                sandbox(channel, model, scaler, sample, variables, sampleConfig["histname"] ,sampleConfig, read.modifyDF )
+                sandbox(channel, model, scaler, sample, variables, sampleConfig["histname"] ,sampleConfig, read.modifyDF, outpath )
 
 
 
     if "hephy.at" in os.environ["HOME"]:
         from Tools.Datacard.produce import Datacard, makePlot
-        from Tools.CutObject.CutObject import Cut        
-        Cut.cutfile = "conf/cuts.json"
-        Datacard.use_config = "datacardConf"
-        D = Datacard(channel, "predicted_prob", "2016", not short, False)
-        D.create(use)
-        makePlot(channel, "predicted_prob", use)
+        from Tools.CutObject.CutObject import Cut
+        from Tools.FakeFactor.FakeFactor import FakeFactor
 
-def sandbox(channel, model, scaler, sample, variables, outname, config = None, modify = None):
+        Datacard.use_config = era + "/datacard_conf"
+        D = Datacard(channel=channel,
+                     variable="predicted_prob",
+                     era=era,
+                     real_est="mc",
+                     add_systematics = not short,
+                     debug=True,
+                     use_cutfile = "conf/cuts_{0}.json".format(era))
+
+        FakeFactor.fractions = "{0}/datacard_conf/fractions/htt_ff_fractions_{0}.root".format(era) 
+
+        D.create(era+"/"+use)
+        makePlot(channel, "ML", era+"/"+use, era, era+"/plots")
+
+def sandbox(channel, model, scaler, sample, variables, outname, config = None, modify = None, outpath = "predictions"):
     # needed because of memory management
     # iterate over chunks of sample and do splitting on the fly
     first = True
@@ -139,8 +170,9 @@ def sandbox(channel, model, scaler, sample, variables, outname, config = None, m
             modify(part, config)
 
         part["THU"] = 1 # Add dummy
+        # Carefull!! Check if splitting is done the same for training. This is the KIT splitting
         folds = [part.query( "abs(evt % 2) != 0 " ).reset_index(drop=True), part.query( "abs(evt % 2) == 0 " ).reset_index(drop=True) ]
-        addPrediction(channel, model.predict( applyScaler(scaler, folds, variables) ), folds, outname, new = first )
+        addPrediction(channel, model.predict( applyScaler(scaler, folds, variables) ), folds, outname, new = first, outpath = outpath )
         
         folds[0].drop(folds[0].index, inplace=True)
         folds[1].drop(folds[1].index, inplace=True)
@@ -149,7 +181,7 @@ def sandbox(channel, model, scaler, sample, variables, outname, config = None, m
         first = False
     del sample
 
-def addPrediction(channel,prediction, df, sample, new = True):
+def addPrediction(channel,prediction, df, sample, new = True, outpath = "predictions"):
 
     for i in xrange( len(df) ):
         for c in prediction[i].columns.values.tolist():
@@ -157,7 +189,8 @@ def addPrediction(channel,prediction, df, sample, new = True):
             
         if i == 0 and new: mode = "w"
         else: mode = "a"
-        df[i].to_root("{0}/{1}-{2}.root".format("predictions",channel, sample), key="TauCheck", mode = mode)
+        # df[i].to_root("{0}/{1}-{2}.root".format("predictions",channel, sample), key="TauCheck", mode = mode)
+        df[i].to_root("{0}/{1}-{2}.root".format(outpath,channel, sample), key="TauCheck", mode = mode)
         prediction[i].drop(prediction[i].index, inplace = True)
 
 def trainScaler(folds, variables):
@@ -173,8 +206,8 @@ def trainScaler(folds, variables):
 def applyScaler(scaler, folds, variables):
     if not scaler: return folds
     newFolds = copy.deepcopy(folds)
-    for fold in newFolds:
-        fold[variables] = scaler.transform( fold[variables] )
+    for i,fold in enumerate(newFolds):
+        fold[variables] = scaler[i].transform( fold[variables] )
     return newFolds
 
 
