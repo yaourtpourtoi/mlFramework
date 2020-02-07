9b4ca7c4fdcbc5c475fab9d9ef36acc2b7bb5108
Oleg Filatov <oleg.filatov@cern.ch>
2020-02-07 08:51:34 +0100

    Emb and qqH only

--- Reader.py
+++ Reader.py
@@ -210,7 +210,7 @@ class Reader():
         if not os.path.exists( sample_info["path"] ):
             print(f'Warning: couldn\'t find {sample_info["path"]}')
             return []        
-        print(f'Loading: {sample_info["histname"]} from {sample_info["path"].split("/")[-1]}, tree = {sample_info["tree_name"]}')
+        print(f'\n------> Loading: {sample_info["histname"]} from {sample_info["path"].split("/")[-1]}, tree = {sample_info["tree_name"]}\n')
         
         DF = self._getDF(sample_path = sample_info["path"], 
                           select = sample_info["select"],
