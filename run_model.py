ba503a19794717b0de58d8ca73bf15eaba376ac8
Oleg Filatov <oleg.filatov@cern.ch>
2020-03-18 10:23:13 +0100

    fix

--- run_model.py
+++ run_model.py
@@ -209,12 +209,12 @@ def sandbox(channel, model, scaler, sample, variables, outname, outpath, config 
                 print(f'\nLeaving them, dropping is set only for {drop_nan_columns}')
             print('**********\n')
             
-        print(config.keys())
         for var_substring, replace_value in config['replace_nan_columns'].items():
             replace_nan_columns = [var_name for var_name in part.columns if var_substring in var_name]
-            print(f'found {np.sum(part[replace_nan_columns].isna()).sum()} NaNs in \"*{var_substring}*\" branches')
-            print(f'will replace them with {replace_value}\n')
-            part[replace_nan_columns] = part[replace_nan_columns].filna(replace_value)
+            if np.sum(part[replace_nan_columns].isna()).sum() != 0:
+                print(f'found {np.sum(part[replace_nan_columns].isna()).sum()} NaNs in \"*{var_substring}*\" branches')
+                print(f'will replace them with {replace_value}\n')
+                part[replace_nan_columns] = part[replace_nan_columns].fillna(replace_value)
             
         if modify:
             modify(part, config)
